9a8e8e53ed95ce8c6073cc4ddd7c714e887e902b
Tácio Dias Palhão Mendes <taciomendes@hotmail.com>
2022-11-13 11:44:00 +0000

    Alternate version

--- Program.cs
+++ Program.cs
@@ -2,39 +2,30 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace FFMPEGPoc
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             DirectoryInfo d = new DirectoryInfo(@"Input");
 
             var files = d.GetFiles();
 
-            Process cli = new Process();
-
-            cli.StartInfo.FileName = "cmd.exe";
-            cli.StartInfo.RedirectStandardInput = true;
-            cli.StartInfo.RedirectStandardOutput = true;
-            cli.StartInfo.UseShellExecute = false;
-
-            cli.Start();
-
             var cmdTemplate = "ffmpeg -i \"Input\\{0}\" -b:a 128K -vn \"Output\\{1}\" ";
-
+            List<string> lines = new List<string>();
             foreach (var file in files)
             {
-                if (file.Name.Split('.')[0].Length > 2) throw new ApplicationException("File Name Contains '.' please rename this file and try again: "+file.Name);
-                var command = String.Format(cmdTemplate, file.Name, file.Name.Split('.')[0] + ".mp3");
-                cli.StandardInput.WriteLine(command);
+                if (file.Name.Split('.').Length > 2) throw new ApplicationException("File Name Contains '.' please rename this file and try again: "+file.Name);
+                lines.Add(String.Format(cmdTemplate, file.Name, file.Name.Split('.')[0] + ".mp3"));
             }
 
-            cli.StandardInput.Flush();
-            cli.StandardInput.Close();
-            Console.WriteLine(cli.StandardOutput.ReadToEnd());
-
+            await File.WriteAllLinesAsync("Output\\command.txt",lines,CancellationToken.None);
+            
         }
     }
 }
